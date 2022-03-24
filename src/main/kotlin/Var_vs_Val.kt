b50b202293630df5142fdb22bf119574ae585c9a
annisa <annisafitriannisa1@gmail.com>
2022-03-24 10:47:38 +0700

    Var vs Val

---
+++ src/main/kotlin/Var_vs_Val.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var varVariable: Int
+    varVariable = 56 //assigning second value to varVariable
+    println("Pemberian nilai pertama pada varVariable = "+varVariable)
+    varVariable = 78 //assigning second value to varVariable
+    println("Pemberian nilai kedua pada varVariable ="+varVariable)
+}
