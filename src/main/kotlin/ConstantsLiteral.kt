9a9accf2e95c607b9fe90cc3a92109752a3b3315
annisa <annisafitriannisa1@gmail.com>
2022-03-24 11:02:26 +0700

    Constants Literal

---
+++ src/main/kotlin/ConstantsLiteral.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val floatLiteral : Float = 178.95F //literal Float
+    println("contoh literal float :"+floatLiteral)
+    val oneBillion : Long = 1000000000L //literal long
+    println("contoh literal long :"+oneBillion)
+    val valHeksa = 0x0F  //literal heksadesimal diawali dengan 0x
+    println("contoh literal heksadesimal :"+valHeksa)
+    val valBinary = 0b00001011 //literal binary diawali dengan 0b
+    println("contoh literal binary :"+valBinary)
+    val notasiKonversional = 123.5e10
+    println("contoh literal floating point number :"+notasiKonversional)
+}
